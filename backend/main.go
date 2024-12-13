2ca4b9fc5ca4d8195130ef13f7cff539ca05dcf6
Hosi121 <takuyakataiwa@icloud.com>
2024-12-13 22:45:56 +0900

    feat: main.goを用意

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,20 @@
-test
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Ginのデフォルトのルーターを作成
+	r := gin.Default()
+
+	// GETリクエストのルートを定義
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	// サーバーを起動
+	r.Run(":8080") // デフォルトポート8080で起動
+}
