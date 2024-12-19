61a42cb340137693d0f884f169d151b126611656
Shiluco <hirap1939@gmail.com>
2024-12-19 22:16:39 +0900

    feat:ボタンの作成

---
+++ frontend/src/app/components/common/button/button.tsx
@@ -0,0 +1,16 @@
+import "@/style/button.scss";
+
+interface ButtonProps {
+  label: string;
+  onClick?: () => void;
+  type: "main" | "minimal";
+}
+export const Button = (props: ButtonProps) =>
+{ 
+  const { label, onClick, type } = props;
+  return (
+    <button className={`button button-${type}`} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
