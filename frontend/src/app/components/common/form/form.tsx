3a74f84940e3bee3658cac67f06fe36a56cfd7dd
Shiluco <hirap1939@gmail.com>
2024-12-21 13:55:24 +0900

    feat:フォームの完成

--- frontend/src/app/components/common/form/form.tsx
+++ frontend/src/app/components/common/form/form.tsx
@@ -1,17 +1,35 @@
-import React from "react";
-
-interface FormProps {
+import '@/style/form.scss';
+interface FormProps
+{
   label: string;
   type: "inline" | "online";
+  setter: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export const Form = (props: FormProps) => {
-  const {label, type } = props;
+  const { label, type, setter } = props;
 
   return (
-    <form  className={`form-${type}`}>
-      <label htmlFor="input-field">{label}</label>
-      <input id="input-field" type="text" placeholder="Enter text here" />
+    <form className={`form-${type}`}>
+      {type === "inline" ? (
+        <>
+          <input
+            id="input-field"
+            type="text"
+            placeholder={label}
+            onChange={(e) => setter(e.target.value)}
+          />
+        </>
+      ) : (
+        <>
+          <label htmlFor="input-field">{`${label}`}</label>
+          <input
+            id="input-field"
+            type="text"
+            onChange={(e) => setter(e.target.value)}
+          />
+        </>
+      )}
     </form>
   );
 };
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,11 @@
 
 import { Button } from "@/app/components/common/button/button";
-import { Form } from "./components/common/form/form";
 
 export default function Home() {
   return (
     <>
       <Button label="Buttonfd" type="main" />
-      <Form label="Form" type="inline" />
+
     </>
   );
 }
