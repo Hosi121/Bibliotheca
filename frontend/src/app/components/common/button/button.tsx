818f202a405e9f16741e856a4a11b875ea10488f
Shiluco <hirap1939@gmail.com>
2024-12-21 14:22:16 +0900

    feat:alertの作成

--- frontend/src/app/components/common/button/button.tsx
+++ frontend/src/app/components/common/button/button.tsx
@@ -3,13 +3,17 @@ import "@/style/button.scss";
 interface ButtonProps {
   label: string;
   onClick?: () => void;
-  type: "main" | "normal" |"minimal";
+  type: "main" | "normal" | "minimal";
+  className?: string; // 外部から追加クラスを受け取るプロパティ
 }
-export const Button = (props: ButtonProps) =>
-{ 
-  const { label, onClick, type } = props;
+
+export const Button = (props: ButtonProps) => {
+  const { label, onClick, type, className = "" } = props;
   return (
-    <button className={`button button-${type}`} onClick={onClick}>
+    <button
+      className={`button button-${type} ${className}`.trim()}
+      onClick={onClick}
+    >
       {label}
     </button>
   );
--- frontend/src/app/components/common/text/text.tsx
+++ frontend/src/app/components/common/text/text.tsx
@@ -1,14 +1,21 @@
 import React from "react";
-import '@/style/text.scss';
+import "@/style/text.scss";
 
 type TextProps = {
-  variant: "h1" | "h2" | "h3" | "body1" | "body2" | "body3";
+  variant?: "h1" | "h2" | "h3" | "body1" | "body2" | "body3";
   bold?: boolean; // 太文字にするオプション
+  className?: string; // 外部からクラス名を追加するオプション
   children: React.ReactNode;
 };
 
-export const Text: React.FC<TextProps> = ({ variant, bold = false, children }) => {
-  const className = `${variant} ${bold ? "bold" : ""}`;
-  return <div className={className}>{children}</div>;
+export const Text = ({
+  variant = "body1",
+  bold = false,
+  className = "",
+  children,
+}: TextProps) => {
+  const computedClassName = `${variant} ${
+    bold ? "bold" : ""
+  } ${className}`.trim();
+  return <div className={computedClassName}>{children}</div>;
 };
-
