4ba4ec7cebb463e573a7ba004724966c36aa41a6
shgnaka <shogonakamurawppt@gmail.com>
2024-12-22 04:42:45 +0900

    refactor: 複合一意制約を指定, borrowing.goで同じ本を同じ日に違う人が借りられないような制約を加えた

--- backend/model/borrowing.go
+++ backend/model/borrowing.go
@@ -4,9 +4,9 @@ import "time"
 
 type Borrowing struct {
 	BorrowingID  uint      `gorm:"primaryKey"`
-	BookID       uint      `gorm:"not null"`
-	UserID       uint      `gorm:"not null"`
-	BorrowDate   time.Time `gorm:"not null"`
+	BookID       uint      `gorm:"not null;uniqueIndex:idx_user_book_date"`
+	UserID       uint      `gorm:"not null;uniqueIndex:idx_user_book_date"`
+	BorrowDate   time.Time `gorm:"not null;uniqueIndex:idx_user_book_date"`
 	DueDate      time.Time `gorm:"not null"`
 	ReturnedDate *time.Time
 	Book         Book      `gorm:"foreignKey:BookID;constraint:OnDelete:RESTRICT"`
