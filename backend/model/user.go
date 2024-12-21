de8208966bb97a1f28a82b24d7b5d19e4663f237
shgnaka <shogonakamurawppt@gmail.com>
2024-12-22 00:18:02 +0900

    refactor: change UserID type int to uint, capitalize initial of the model name and field name

--- backend/model/user.go
+++ backend/model/user.go
@@ -1,13 +1,10 @@
 package model
 
-import "time"
-
-type users struct {
-	user_id int `gorm:"primary_key"`
-	email string `"gorm:unique;not null"`
-	hashed_password string `gorm:"not null"`
-	username string `gorm:"not null"`//unique????
-	role string `gorm:"type:string"`
-	created_at time.Time
-	updated_at time.Time
+type User struct {
+	UserID             uint   `gorm:"primaryKey"`
+	Email          string `gorm:"unique;not null"`
+	HashedPassword string `gorm:"not null"`
+	Username       string `gorm:"not null"`
+	Role           string `gorm:"type:string;not null"`
+	BaseModel
 }
