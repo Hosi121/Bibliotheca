bd2a4e328704b1a7f3d916e7eb369bdc3d4b7315
Shiluco <hirap1939@gmail.com>
2024-12-21 15:18:38 +0900

    refactor:デモ画面の作成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,35 +10,35 @@ export default function Home() {
         id={1}
         title="Title"
         ISBN={1234567890123}
-        buttonLabel="削除"
+        buttonLabel="借りる"
         onClick={() => {}}
       />
       <BookColumn
         id={2}
         title="Title"
         ISBN={1234567890123}
-        buttonLabel="削除"
+        buttonLabel="借りる"
         onClick={() => {}}
       />
       <BookColumn
         id={3}
         title="Title"
         ISBN={1234567890123}
-        buttonLabel="削除"
+        buttonLabel="借りる"
         onClick={() => {}}
       />
       <BookColumn
         id={4}
         title="Title"
         ISBN={1234567890123}
-        buttonLabel="削除"
+        buttonLabel="借りる"
         onClick={() => {}}
       />
       <BookColumn
         id={5}
         title="Title"
         ISBN={1234567890123}
-        buttonLabel="削除"
+        buttonLabel="借りる"
         onClick={() => {}}
       />
 
