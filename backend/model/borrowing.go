fd25dec9e9efd988c76074b7050871cd783804f2
shgnaka <shogonakamurawppt@gmail.com>
2024-12-22 14:45:14 +0900

    feature: add GORM Borrowing CRUD control on Borrowing.go, need more definition about Read

--- backend/model/borrowing.go
+++ backend/model/borrowing.go
@@ -1,6 +1,9 @@
 package model
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Borrowing struct {
 	BorrowingID  uint      `gorm:"primaryKey"`
@@ -12,4 +15,42 @@ type Borrowing struct {
 	Book         Book      `gorm:"foreignKey:BookID;constraint:OnDelete:RESTRICT"`
 	User         User      `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
 	BaseModel
+}
+
+// CreateBorrowing creates a new borrowing record in the database
+func CreateBorrowing(db *gorm.DB, borrowing *Borrowing) error {
+	// db.Create adds a new record in the database for the given borrowing struct
+	return db.Create(borrowing).Error
+}
+
+// GetBorrowingByID retrieves a borrowing record by its ID
+func GetBorrowingByID(db *gorm.DB, borrowingID uint) (*Borrowing, error) {
+	var borrowing Borrowing
+	// db.First fetches the first record matching the primary key
+	if err := db.First(&borrowing, borrowingID).Error; err != nil {
+		return nil, err // Return an error if the borrowing record is not found
+	}
+	return &borrowing, nil // Return the found borrowing record
+}
+
+// GetAllBorrowings retrieves all borrowing records from the database
+func GetAllBorrowings(db *gorm.DB) ([]Borrowing, error) {
+	var borrowings []Borrowing
+	// db.Find fetches all records from the borrowings table
+	if err := db.Find(&borrowings).Error; err != nil {
+		return nil, err // Return an error if the query fails
+	}
+	return borrowings, nil // Return the list of borrowings
+}
+
+// UpdateBorrowing updates an existing borrowing record's information
+func UpdateBorrowing(db *gorm.DB, borrowing *Borrowing) error {
+	// db.Save updates the given borrowing record in the database
+	return db.Save(borrowing).Error
+}
+
+// DeleteBorrowing deletes a borrowing record by its ID
+func DeleteBorrowing(db *gorm.DB, borrowingID uint) error {
+	// db.Delete removes the record with the given primary key
+	return db.Delete(&Borrowing{}, borrowingID).Error
 }
